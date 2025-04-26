14de2aa0d3ea69133809b219322ec5aa1bffba6f
Bruno Dantas <brunoodantas7@gmail.com>
2025-04-25 23:05:44 -0300

    Initial commit.

---
+++ src/main/java/com/dantas/springupskilling/DsSpringUpskillingApplication.java
@@ -0,0 +1,13 @@
+package com.dantas.springupskilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsSpringUpskillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsSpringUpskillingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dantas/springupskilling/DsSpringUpskillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dantas.springupskilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsSpringUpskillingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
