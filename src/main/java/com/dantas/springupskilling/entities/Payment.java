d1165589526d18d433428b1854f57c3508e8c84b
Bruno Dantas <brunoodantas7@gmail.com>
2025-04-29 11:48:14 -0300

    add: Mapping domain model oneToOne on ORM.

--- src/main/java/com/dantas/springupskilling/entities/Payment.java
+++ src/main/java/com/dantas/springupskilling/entities/Payment.java
@@ -1,7 +1,10 @@
 package com.dantas.springupskilling.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 
@@ -10,23 +13,16 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
 
-
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
-
+    @OneToOne
+    @MapsId
+    private Order order;
 }
--- src/main/java/com/dantas/springupskilling/entities/User.java
+++ src/main/java/com/dantas/springupskilling/entities/User.java
@@ -1,15 +1,14 @@
 package com.dantas.springupskilling.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
