c722d6ab0b6e23e861dde974aabfd06856071c2d
Bruno Dantas <brunoodantas7@gmail.com>
2025-04-29 22:04:01 -0300

    add: DB seeding.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+ INSERT INTO tb_categoria (descricao) VALUES ('Curso');
+INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
+INSERT INTO tb_participante (email, nome) VALUES ('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante (email, nome) VALUES ('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante (email, nome) VALUES ('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante (email, nome) VALUES ('Teresa Silva', 'teresa@gmail.com');
+INSERT INTO tb_atividade (preco, descricao, nome, categoria_id) VALUES (80.00, 'Aprenda HTML de forma prática', 'Curso de HTML', 1);
+INSERT INTO tb_atividade (preco, descricao, nome, categoria_id) VALUES (50.00, 'Controle versões de seus projetos', 'Oficina de Github', 2);
+INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (1, 1);
+INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (1, 2);
+INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (1, 3);
+INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (2, 1);
+INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (2, 3);
+INSERT INTO tb_atividade_participante (atividade_id, participante_id) VALUES (2, 4);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITHOUT TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2017-09-25T11:00:00Z', 1);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITHOUT TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2017-09-25T18:00:00Z', 2);
+INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITHOUT TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2017-09-26T11:00:00Z', 2);
