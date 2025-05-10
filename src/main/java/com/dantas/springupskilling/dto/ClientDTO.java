ebebab2157636fb1071e6abf5339de665d329941
Bruno Dantas <brunoodantas7@gmail.com>
2025-05-09 22:36:41 -0300

    add: API exception handler working with bean validation.

--- src/main/java/com/dantas/springupskilling/dto/ClientDTO.java
+++ src/main/java/com/dantas/springupskilling/dto/ClientDTO.java
@@ -1,10 +1,19 @@
 package com.dantas.springupskilling.dto;
 
 import com.dantas.springupskilling.entity.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
-public record ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+public record ClientDTO(
+        Long id,
+        @NotBlank String name,
+        String cpf, Double income,
+        @NotNull @PastOrPresent LocalDate birthDate,
+        Integer children) {
 
     public Client toEntity(){
         return new Client(this);
--- src/main/java/com/dantas/springupskilling/exception/AppExceptionHandler.java
+++ src/main/java/com/dantas/springupskilling/exception/AppExceptionHandler.java
@@ -3,10 +3,13 @@ package com.dantas.springupskilling.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
+import java.util.Map;
 
 @ControllerAdvice
 public class AppExceptionHandler {
@@ -29,6 +32,38 @@ public class AppExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
 
         problemDetail.setTitle("Error occurs on database operation");
+        problemDetail.setProperty("uri", request.getRequestURI());
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request){
+
+        ProblemDetail problemDetail = ProblemDetail
+                .forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
+
+        problemDetail.setTitle("Validation error on request body");
+        problemDetail.setProperty("uri", request.getRequestURI());
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        problemDetail.setProperty("errorsList", exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> Map.of("field", fieldError.getField(), "errorMessage", fieldError.getDefaultMessage()))
+        );
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException exception, HttpServletRequest request){
+
+        ProblemDetail problemDetail = ProblemDetail.
+                forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
+
+        problemDetail.setTitle("Invalid request body");
         problemDetail.setProperty("uri", request.getRequestURI());
         problemDetail.setProperty("timestamp", Instant.now());
 
