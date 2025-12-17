f138d11b7755632583a95fec84ecb9de6fecfade
Bruno Dantas <brunoodantas7@gmail.com>
2025-12-17 00:00:31 -0300

    chore: Product DTO validation.

--- src/main/java/com/dantas/springupskilling/dto/ProductDTO.java
+++ src/main/java/com/dantas/springupskilling/dto/ProductDTO.java
@@ -3,6 +3,7 @@ package com.dantas.springupskilling.dto;
 import com.dantas.springupskilling.entities.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -24,7 +25,8 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
-    
+
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     
