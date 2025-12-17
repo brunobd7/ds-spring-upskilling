246eda098574d3f68cb85e2afc2210603fad783d
Bruno Dantas <brunoodantas7@gmail.com>
2025-12-17 00:21:52 -0300

    chore: Allow requests from any user to list products.

--- src/main/java/com/dantas/springupskilling/controllers/ProductController.java
+++ src/main/java/com/dantas/springupskilling/controllers/ProductController.java
@@ -21,7 +21,7 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')") // TODO REMOVIDO PARA ENTREGA DESAFIO
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
